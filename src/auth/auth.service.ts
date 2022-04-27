18bfcb6bd14130004d8e24e95e9546ae3dea0e7c
N'GUESSAN Ahouatho Omaël <omaelberiz@outlook.fr>
2022-04-27 16:40:38 +0000

    Mise en place du service d'authentification

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
-  signin() {
-    return { msg: "I'm sign in" };
+  constructor(private prisma: PrismaService) {}
+
+  /**
+   * user sign up service
+   *
+   * @param dto AuthDto
+   * @returns
+   */
+  async signup(dto: AuthDto) {
+    try {
+      // generate the password hash
+      const hash = await argon.hash(dto.password);
+      //save the new user in the database
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash: hash,
+        },
+      });
+
+      delete user.hash;
+
+      //return saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
   }
 
-  signup() {
-    return { msg: "I'm sign up" };
+  /**
+   * user sign in service
+   *
+   * @param dto AuthDto
+   * @returns Object User|Throw
+   */
+  async signin(dto: AuthDto) {
+    // find the user
+    const user = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+    // if user does not exist throw exception
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+
+    // compare password
+    const passwordMatches = await argon.verify(user.hash, dto.password);
+    // if password is not correct throw exception
+    if (!passwordMatches) throw new ForbiddenException('Credentials incorrect');
+
+    // send back the user
+    return user;
   }
 }
