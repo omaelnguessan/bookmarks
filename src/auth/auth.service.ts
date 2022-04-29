5a8ec9c7c1b2d2474f581db59def2d63a58526a1
N'GUESSAN Ahouatho Omaël <omaelberiz@outlook.fr>
2022-04-29 16:18:56 +0000

    Mise en place du système d'authentification

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,16 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   /**
    * user sign up service
@@ -28,8 +34,8 @@ export class AuthService {
 
       delete user.hash;
 
-      //return saved user
-      return user;
+      // send back the user
+      return this.signToken(user.id, user.email);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
@@ -60,6 +66,35 @@ export class AuthService {
     if (!passwordMatches) throw new ForbiddenException('Credentials incorrect');
 
     // send back the user
-    return user;
+    return this.signToken(user.id, user.email);
+  }
+
+  /**
+   *user sign token
+   *
+   * @param userId string
+   * @param email string
+   * @returns {access_token} Promise<{access_token: string}>
+   */
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    //config payload object
+    const payload = {
+      sub: userId,
+      email: email,
+    };
+
+    const secret = this.config.get('JWT_SECRET');
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: secret,
+    });
+
+    return {
+      access_token: token,
+    };
   }
 }
