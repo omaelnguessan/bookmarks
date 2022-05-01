58df3d515d49d8930c0e53a5655a7a3d31b9eb86
N'GUESSAN Ahouatho Omaël <omaelberiz@outlook.fr>
2022-05-01 19:58:59 +0000

    Add config for pactum js

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,98 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import * as pactum from 'pactum';
+import { PrismaService } from '../src/prisma/prisma.service';
+import { AppModule } from '../src/app.module';
+import { AuthDto } from 'src/auth/dto';
 
-describe('AppController (e2e)', () => {
+describe('App e2e', () => {
   let app: INestApplication;
-
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  let prisma: PrismaService;
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
     await app.init();
+    await app.listen(3333);
+
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
+    pactum.request.setBaseUrl('http://localhost:3333');
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(() => {
+    app.close();
+  });
+
+  /**
+   * Auth
+   */
+  describe('Auth', () => {
+    const dto: AuthDto = {
+      email: 'omaelberiz@gmail.com',
+      password: '123',
+    };
+
+    describe('Signup', () => {
+      it('Should sign up', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+      });
+    });
+
+    describe('Signin', () => {
+      it('Should sign in', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(dto)
+          .expectStatus(200);
+      });
+    });
+  });
+
+  /**
+   * User
+   */
+  describe('User', () => {
+    describe('Get current user', () => {
+      it.todo('Should Get current user');
+    });
+
+    describe('Edit user', () => {
+      it.todo('Should Edit user');
+    });
+  });
+
+  /**
+   * bookmarks
+   */
+  describe('Bookmarks', () => {
+    describe('Create Bookmark', () => {
+      it.todo('Should Create Bookmark');
+    });
+
+    describe('Get Bookmark', () => {
+      it.todo('Should Get Bookmark');
+    });
+
+    describe('Get Bookmark by id', () => {
+      it.todo('Should Get Bookmark by id');
+    });
+
+    describe('Edit Bookmark', () => {
+      it.todo('Should Edit Bookmark');
+    });
+
+    describe('Delete Bookmark', () => {
+      it.todo('Should Delete Bookmark');
+    });
   });
 });
